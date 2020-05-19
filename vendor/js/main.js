8e7d899471107cf8d0bdc54b9bc9c3a5a9a4b59c
someone <someone.someplace.com>
2020-05-20 01:23:54 +0200

    first commit

---
+++ vendor/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+});
+
+(function ($) {
+
+    var previousScroll = 20;
+        // scroll functions
+        $(window).scroll(function(e) {
+        
+            // add/remove class to navbar when scrolling to hide/show
+            var scroll = $(window).scrollTop();
+            if (scroll >= previousScroll) {
+                $('.navbar').addClass("navbar-hide");
+            
+            }else if (scroll < previousScroll) {
+                $('.navbar').removeClass("navbar-hide");
+            }
+            previousScroll = scroll;
+        
+        });
+        
+})(jQuery);    
